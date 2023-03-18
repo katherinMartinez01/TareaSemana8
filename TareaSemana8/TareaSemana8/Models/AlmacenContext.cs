04e0a05da21cf1c995a04184df576ad15015c277
katherin martinez <50372@DESKTOP-B9OA3JG>
2023-03-18 10:31:49 -0600

    tarea8 ya terminada

---
+++ TareaSemana8/TareaSemana8/Models/AlmacenContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace TareaSemana8.Models;
+
+public partial class AlmacenContext : DbContext
+{
+    public AlmacenContext()
+    {
+    }
+
+    public AlmacenContext(DbContextOptions<AlmacenContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=.;Database=Almacen;Trusted_Connection=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Producto__3214EC07934F21B6");
+
+            entity.ToTable("Producto");
+
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Precio).HasColumnType("decimal(16, 2)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ TareaSemana8/TareaSemana8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TareaSemana8;
+using TareaSemana8.Models;
+
+Producto producto = new Producto();
+using (AlmacenContext db = new AlmacenContext())
+{
+    Console.WriteLine("Por Favor Ingrese el Nombre");
+    producto.Nombre = Console.ReadLine();
+    Console.WriteLine("Por Favor Ingrese la Descripcion");
+    producto.Descripcion = Console.ReadLine();
+    Console.WriteLine("Por Favor Ingrese el Precio");
+    producto.Precio = decimal.Parse(Console.ReadLine());
+    Console.WriteLine("Por Favor Ingrese el Stock");
+    producto.Stock = int.Parse(Console.ReadLine());
+    db.Add(producto);
+    db.SaveChanges();
+    Console.WriteLine("¡¡Su Producto Fue Ingresado  Exitosamente!!\n");
+
+    var lista = db.Productos.ToList();
+    foreach (var mostrar in lista)
+    {
+        Console.WriteLine("\nNombre:  " + mostrar.Nombre);
+        Console.WriteLine("Descripcion: " + mostrar.Descripcion);
+        Console.WriteLine("Precio  " + mostrar.Precio);
+        Console.WriteLine("Stock " + mostrar.Stock);
+    }
+
+}
